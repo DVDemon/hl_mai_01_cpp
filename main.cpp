0a499e43d5a2ab1eb3e4ac6562a2d5494ed1a6f5
Dmitriy Dzyuba <ddzuba@yandex.ru>
2021-02-07 09:45:24 +0000

    some changes

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main([[maybe_unused]]int argc,[[maybe_unused]] char*argv[])
 {
-
+    std::cout << "Hello world!" << std::endl;
     std::cout << get_config_string() << std::endl;
     std::cout << "result:" << complex_calculation(2,2) << std::endl;
     return 0;
